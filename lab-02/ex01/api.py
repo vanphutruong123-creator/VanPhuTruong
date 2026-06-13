368e1e006c3b62d8947ce268356f01393aafac1b
VanPhuTruong <vanphutruong123@gmail.com>
2026-06-13 23:00:46 +0700

    [add] lab-02 playfair

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -1,3 +1,4 @@
+from cipher.playfair import PlayfairCipher
 from cipher.railfence import RailFenceCipher
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
@@ -67,6 +68,27 @@ def railfence_decrypt():
     cipher_text = data['cipher_text']
     key = int(data['key'])
     decrypted_text = railfence_cipher.rail_fence_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
+
+    # ==========================================
+# 4. PLAYFAIR CIPHER ALGORITHM (Thêm mới - Trang 85)
+# ==========================================
+playfair_cipher = PlayfairCipher()
+
+@app.route('/api/playfair/encrypt', methods=['POST'])
+def playfair_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/playfair/decrypt', methods=['POST'])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, key)
     return jsonify({'decrypted_text': decrypted_text})
 # ==========================================
 # MAIN FUNCTION
